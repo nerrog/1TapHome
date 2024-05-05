252f8e3872b028a000bc1a8987da2c3e151d5870
nerrog <tnk@nerrog.net>
2024-05-06 01:31:40 +0900

    [UPDATE] Minecraft 1.20.4 home送信時オーバーレイテキスト表示追加

---
+++ README.md
@@ -0,0 +1,7 @@
+# 1TapHome
+![fabric](https://img.shields.io/badge/mod%20loader-fabric-d64541?style=flat-square)
+![mod](https://img.shields.io/badge/environment-client-1976d2?style=flat-square)
+
+A client mod that sends the `/home` command by simply pressing the H key on the keyboard
+- Fabric MC 1.20.4 (loader 0.15.0+)
+- Key bindings can be changed.
--- src/main/java/net/nerrog/taphome/client/taphome.java
+++ src/main/java/net/nerrog/taphome/client/taphome.java
@@ -1,10 +1,15 @@
 package net.nerrog.taphome.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.Objects;
@@ -28,7 +33,13 @@ public class taphome implements ClientModInitializer {
         //KeyEvent
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (keyBinding.wasPressed()){
-                Objects.requireNonNull(client.player).sendChatMessage("/home");
+                try {
+                    if (client.player == null) return;
+                    client.player.networkHandler.sendChatCommand("home");
+                    Objects.requireNonNull(client.player).sendMessage(Text.of(I18n.translate("screen.1taphome.go_home")), true);
+                } catch (Exception e) {
+                    Objects.requireNonNull(client.player).sendMessage(Text.of("§c[1TapHome]error!\n" + e.getMessage()), false);
+                }
             }
         });
     }
